01266dab2c1a686861a391b7f927cf753ebeab5d
Stefan Probst <stefanprobst@posteo.de>
2021-09-03 20:46:21 +0200

    add micromark-util-events-to-acorn dependency

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import 'micromark-util-events-to-acorn'
 
 export default function Home() {
   return (
